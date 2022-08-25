2b1c2cb2cc7cc67406ecf4c148bd33dba9d592c2
email <helu1020@gmail.com>
2022-08-25 10:22:59 -0500

    Creación de los atributos y constructor de la clase Empleado

--- src/main/java/com/co/udea/mintic/ChameleonApp/Entities/Empleado.java
+++ src/main/java/com/co/udea/mintic/ChameleonApp/Entities/Empleado.java
@@ -1,4 +1,18 @@
 package com.co.udea.mintic.ChameleonApp.Entities;
 
 public class Empleado {
+
+    private String nombreEmpleado;
+    private String correoEmpleado;
+    private String empresaEmpleado;
+    private String rolEmpleado;
+
+    public Empleado(String nombreEmpleado, String correoEmpleado, String empresaEmpleado, String rolEmpleado){
+        this.nombreEmpleado=nombreEmpleado;
+        this.correoEmpleado=correoEmpleado;
+        this.empresaEmpleado=empresaEmpleado;
+        this.rolEmpleado=rolEmpleado;
+    }
+    //Espacio para Getter y Setter
+
 }
