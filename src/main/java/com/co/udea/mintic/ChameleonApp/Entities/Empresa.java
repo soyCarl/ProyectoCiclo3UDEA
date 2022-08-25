9c79209288c0705de826b00c5f6532f4d7882482
Armando Colón Mendinueta <musicarcolmen@gmail.com>
2022-08-25 05:08:44 -0500

    Creación de atributos y métodos getters & setters en el modelo Empresa

--- src/main/java/com/co/udea/mintic/ChameleonApp/Entities/Empresa.java
+++ src/main/java/com/co/udea/mintic/ChameleonApp/Entities/Empresa.java
@@ -1,5 +1,42 @@
 package com.co.udea.mintic.ChameleonApp.Entities;
 
 public class Empresa {
+    /* ------- Atributos ------- */
+    private String nombreEmpresa;
+    private String direccionEmpresa;
+    private String telefonoEmpresa;
+    private String nitEmpresa;
+    /* ------- Atributos ------- */
+
+
+    /* ------- Getters & Setters ------- */
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+    public String getDireccionEmpresa() {
+        return direccionEmpresa;
+    }
+    public void setDireccionEmpresa(String direccionEmpresa) {
+        this.direccionEmpresa = direccionEmpresa;
+    }
+    public String getTelefonoEmpresa() {
+        return telefonoEmpresa;
+    }
+    public void setTelefonoEmpresa(String telefonoEmpresa) {
+        this.telefonoEmpresa = telefonoEmpresa;
+    }
+    public String getNitEmpresa() {
+        return nitEmpresa;
+    }
+    public void setNitEmpresa(String nitEmpresa) {
+        this.nitEmpresa = nitEmpresa;
+    }
+    /* ------- Getters & Setters ------- */
+
+
+
 
 }
