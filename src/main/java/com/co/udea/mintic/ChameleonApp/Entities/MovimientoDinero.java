6dd9cfc53ceda4c59ee2e63f5a1e42e2b722922c
Diego Tobon <cdtobon19@gmail.com>
2022-08-25 21:09:10 -0500

    creacion de clase movimientoDinero

--- src/main/java/com/co/udea/mintic/ChameleonApp/Entities/MovimientoDinero.java
+++ src/main/java/com/co/udea/mintic/ChameleonApp/Entities/MovimientoDinero.java
@@ -21,16 +21,7 @@ public class MovimientoDinero {
     public MovimientoDinero() {
     }
 
-    public MovimientoDinero(Long id, String concepto, Float monto, Empleado empleado, Date fechaCreacion, Date fechaActualizacion) {
-        this.id = id;
-        this.concepto = concepto;
-        this.monto = monto;
-        this.empleado = empleado;
-        this.fechaCreacion = fechaCreacion;
-        this.fechaActualizacion = fechaActualizacion;
-    }
-
-    public Long getId() {
+     public Long getId() {
         return id;
     }
 
