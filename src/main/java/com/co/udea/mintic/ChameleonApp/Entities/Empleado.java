f124a6d71cb99b2ebfe72def596c1aec31faeaef
briggid <brisierrav@gmail.com>
2022-08-26 16:20:50 -0500

    Construcción de los Get y Set de la clase Empleado.

--- src/main/java/com/co/udea/mintic/ChameleonApp/Entities/Empleado.java
+++ src/main/java/com/co/udea/mintic/ChameleonApp/Entities/Empleado.java
@@ -16,6 +16,38 @@ public class Empleado {
     //Espacio para Getter y Setter
 
 
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getCorreoEmpleado() {
+        return correoEmpleado;
+    }
+
+    public void setCorreoEmpleado(String correoEmpleado) {
+        this.correoEmpleado = correoEmpleado;
+    }
+
+    public String getEmpresaEmpleado() {
+        return empresaEmpleado;
+    }
+
+    public void setEmpresaEmpleado(String empresaEmpleado) {
+        this.empresaEmpleado = empresaEmpleado;
+    }
+
+    public String getRolEmpleado() {
+        return rolEmpleado;
+    }
+
+    public void setRolEmpleado(String rolEmpleado) {
+        this.rolEmpleado = rolEmpleado;
+    }
+
     @Override
     public String toString() {
         return "Empleado{" +
